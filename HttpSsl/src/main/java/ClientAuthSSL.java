dccd45ddb65a25eb8e7651c7c9834c29af778c72
Karl Nicholas <karl.nicholas@outlook.com>
2017-12-16 16:29:26 -0800

    Updates things.

--- HttpSsl/src/main/java/ClientAuthSSL.java
+++ HttpSsl/src/main/java/ClientAuthSSL.java
@@ -32,7 +32,6 @@ import javax.net.ssl.SSLContext;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
